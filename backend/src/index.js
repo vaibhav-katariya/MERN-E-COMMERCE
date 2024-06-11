50912df55bc0e50cd31711e82df0e9c97d807a4d
git-demo2 <vaibhavahir4456@gmail.com>
2024-06-11 16:56:11 +0530

    connect db

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,12 @@ dotenv.config({
   path: "./.env",
 });
 
-// connectDB()
-//   .then(() => {
-//     app.listen(process.env.PORT || 8080, () => {
-//       console.log(`Server is running on port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch(() => {
-//     console.log("Error connecting to database");
-//   });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Error connecting to database");
+  });
