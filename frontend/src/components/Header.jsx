e8e929c2724ff25ae17fb596646fcf7709e86689
git-demo2 <vaibhavahir4456@gmail.com>
2024-06-15 11:54:11 +0530

    make better frontend degine

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,33 @@
-import React, { useTransition } from "react";
+import * as React from "react";
 import { FaSearch } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { logout } from "../store/userSlice";
 import { Link, useNavigate } from "react-router-dom";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
 
 const Header = () => {
   const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const logoutHandler = async () => {
     try {
@@ -36,29 +55,92 @@ const Header = () => {
         </button>
       </div>
       <div className="flex items-center gap-5">
-        {user && (
-          <div className="overflow-hidden h-[2.5rem] w-[2.5rem] border-zinc-700 p-[0.2rem] border-[1px] rounded-full">
-            <img
-              className="h-full w-full rounded-full object-cover"
-              src={user?.avatar}
-              alt=""
-            />
-          </div>
-        )}
-        {user !== null ? (
-          <div
-            onClick={logoutHandler}
-            className="py-1 text-white font-semibold cursor-pointer px-3 bg-zinc-700 rounded-lg"
-          >
-            Logout
-          </div>
-        ) : (
-          <Link to={"/login"}>
-            <div className="py-1 text-white font-semibold cursor-pointer px-3 bg-zinc-700 rounded-lg">
-              Login
-            </div>
-          </Link>
-        )}
+        <Box
+          sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
+        >
+          <Tooltip title="Account settings">
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{ ml: 2 }}
+              aria-controls={open ? "account-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+            >
+              {user ? (
+                <Avatar sx={{ width: 32, height: 32 }}>
+                  <img
+                    className="h-full w-full object-cover"
+                    src={user?.avatar}
+                    alt={user?.username}
+                  />
+                </Avatar>
+              ) : (
+                <Avatar sx={{ width: 32, height: 32 }} />
+              )}
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&::before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem onClick={handleClose}>
+            <Avatar /> Profile
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Settings fontSize="small" />
+            </ListItemIcon>
+            Settings
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            {user !== null ? (
+              <div onClick={logoutHandler} className="">
+                Logout
+              </div>
+            ) : (
+              <Link to={"/login"}>
+                <div className="">Login</div>
+              </Link>
+            )}
+          </MenuItem>
+        </Menu>
       </div>
     </div>
   );
