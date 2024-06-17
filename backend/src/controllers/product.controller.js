9737fd63f7238c6f5dc51b238832371dc9c79d22
git-demo2 <vaibhavahir4456@gmail.com>
2024-06-17 18:22:13 +0530

    getOwnerProduct , getProductById , getAllProduct controller

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { Product } from "../model/product.model.js";
+import { User } from "../model/user.model.js";
 import { fileUploadOnCloudinary } from "../utils/fileUploader.js";
 
 const uploadProduct = async (req, res) => {
@@ -57,4 +58,86 @@ const uploadProduct = async (req, res) => {
   }
 };
 
-export { uploadProduct };
+const getOwnerProducts = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const data = await Product.find({ owner: user._id }).populate({
+      path: "owner",
+      select: "-password -refreshToken",
+    });
+
+    if (!data) {
+      return res.status(404).json({
+        message: "No products found",
+      });
+    }
+
+    res.status(200).json({
+      message: "products fetched successfully",
+      data,
+    });
+  } catch (error) {
+    console.log("error while get product", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id).populate({
+      path: "owner",
+      select: "-password -refreshToken",
+    });
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      message: "Product fetched successfully",
+      product,
+    });
+  } catch (error) {
+    console.log("error while get product by id", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const getAllProduct = async (req, res) => {
+  try {
+    const product = await Product.find({}).populate({
+      path: "owner",
+      select: "-password -refreshToken",
+    });
+    if (!product) {
+      return res.status(404).json({
+        message: "No products found",
+      });
+    }
+    res.status(200).json({
+      message: "All products fetched successfully",
+      product,
+    });
+  } catch (error) {
+    console.log("error while get all product", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export { uploadProduct, getOwnerProducts, getProductById, getAllProduct };
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middlerware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { uploadProduct } from "../controllers/product.controller.js";
+import {
+  getAllProduct,
+  getOwnerProducts,
+  getProductById,
+  uploadProduct,
+} from "../controllers/product.controller.js";
 
 const router = Router();
 
@@ -12,5 +17,7 @@ router
     upload.fields([{ name: "productImage", maxCount: 1 }]),
     uploadProduct
   );
-
+router.route("/getOwnerProduct/:username").get(getOwnerProducts);
+router.route("/getProductById/:id").get(getProductById);
+router.route("/getAllProduct").get(getAllProduct);
 export default router;
