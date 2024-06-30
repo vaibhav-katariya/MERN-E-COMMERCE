5495112d7c03cbec0e8210e0b8207cfe91856c36
git-demo2 <vaibhavahir4456@gmail.com>
2024-06-30 15:56:55 +0530

    add mail function

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import {
   fileDeleteOnCloudinary,
   fileUploadOnCloudinary,
 } from "../utils/fileUploader.js";
+import { sendEmail } from "../utils/sendMail.js";
 
 const genAccessTokenAndRefreshToken = async (userId) => {
   try {
@@ -69,6 +70,12 @@ const registerUser = async (req, res) => {
     });
 
     const createdUser = await User.findById(user._id).select("-password");
+
+    await sendEmail({
+      email: createdUser.email,
+      subject: "Welcome to the app",
+      html: `<h1>Welcome to the app</h1>`,
+    })
 
     if (!createdUser) {
       return res.status(500).json({
