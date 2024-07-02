91cbe64bf82009f4e7dda29dae322981a90e914a
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-02 21:34:23 +0530

    add order model

--- backend/src/model/order.model.js
+++ backend/src/model/order.model.js
@@ -1,5 +1,100 @@
 import mongoose from "mongoose";
+import { Product } from "./product.model";
 
-const orderSchema = new mongoose.Schema({}, { timestamps: true });
+const orderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+
+      state: {
+        type: String,
+        required: true,
+      },
+
+      country: {
+        type: String,
+        required: true,
+      },
+      pinCode: {
+        type: Number,
+        required: true,
+      },
+      phoneNo: {
+        type: Number,
+        required: true,
+      },
+    },
+    orderItems: [
+      {
+        price: {
+          type: Number,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    paymentInfo: {
+      id: {
+        type: String,
+        required: true,
+      },
+      status: {
+        type: String,
+        required: true,
+      },
+    },
+    paidAt: {
+      type: Date,
+      required: true,
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    orderStatus: {
+      type: String,
+      required: true,
+      default: "Processing",
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
 
 export const Order = mongoose.model("Order", orderSchema);
