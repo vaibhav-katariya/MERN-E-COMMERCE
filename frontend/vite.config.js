8281371eb7cd040fbfa98c79ba872ace8efdc505
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-22 11:31:33 +0530

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://mern-e-commerce-bqou.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
