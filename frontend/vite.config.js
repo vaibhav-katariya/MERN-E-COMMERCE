86d0e2a6db164478bb976dfb355b305373134299
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-24 11:43:43 +0530

    ready to deploy frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5050",
+        target: "https://mern-e-commerce-fgnj.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
